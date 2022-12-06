82d15840adb86a495a325d73544717a5c3d8e0d4
shavaxzz <118012658+shavaxzz@users.noreply.github.com>
2022-12-06 00:06:00 -0600

    Add files via upload

---
+++ 1nomultiplos.js
@@ -0,0 +1,17 @@
+
+var multiplo = prompt("ingrese numero no multiplos");
+var mult;
+for (mult=0; mult<=multiplo; mult++){
+
+if(mult % 7==0){
+    console.log()
+
+}
+else if(mult % 3==0){
+    console.log()
+} else{
+console.log(mult);
+}
+
+    
+}
---
+++ 3numeromayor.js
@@ -0,0 +1,11 @@
+
+var numerouno = prompt("ingrese numero");
+var numerodos = prompt("ingrese numero");
+var numerotres = prompt("ingrese numero");
+var numerocuatro = prompt("ingrese numero");
+var numerocinco = prompt("ingrese numero");
+
+var mayor = Math.max(numerouno,numerodos,numerotres,numerocuatro,numerocinco);
+
+console.log(mayor);
+
